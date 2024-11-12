50020fdf862d5244ab5324eea0796a2feb91603b
Daniel <daniel.rufes@gmail.com>
2024-11-12 19:28:37 +0100

    Ejercicios

---
+++ DRB-UD07-Act01/Ej1.js
@@ -0,0 +1,32 @@
+var peticion = new XMLHttpRequest();
+      function obtenerDatosServidor() {
+        peticion.onreadystatechange = function () {
+          if (peticion.readyState == 4 && peticion.status == 200) {
+            var datosJson = JSON.parse(peticion.responseText);
+            numempleados = datosJson.length;
+            for (i = 0; i < datosJson.length; i++) {
+              document.getElementById("caja").innerHTML +=
+                "<tr><td>" +
+                datosJson[i].id +
+                "</td><td>" +
+                datosJson[i].nombre +
+                "</td><td>" +
+                datosJson[i].edad +
+                "</td><td>" +
+                datosJson[i].cargo +
+                "</td><td>" +
+                datosJson[i].contratado +
+                "</td><td><button onclick='borrarEmpleado(" +
+                datosJson[i].id +
+                ");'>Modificar</button><button onclick='borrarEmpleado(" +
+                datosJson[i].id +
+                ");'" +
+                (numempleados == 1 ? "disabled" : "") +
+                ">Eliminar</button></td></tr>";
+            }
+          }
+        };
+        peticion.open("GET", "http://test-api.jtarrega.es/api/empleados", true);
+        peticion.send();
+      }
+      obtenerDatosServidor();
